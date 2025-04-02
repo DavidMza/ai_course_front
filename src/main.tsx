d6926b6da38a64e5ed6a830e957ce887c25961d4
david <david.senese@gamil.com>
2025-04-02 14:17:25 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-const baseName = import.meta.env.BASE_URL;
-console.log('ACTUAL BASENAME USED:', baseName);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
